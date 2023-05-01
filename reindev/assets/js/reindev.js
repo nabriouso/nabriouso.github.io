74b5eda51e35bf28c137a085f7e9badfe438e950
Nabrious <nabrious@icloud.com>
2023-05-01 23:44:23 +0100

    woohoo

---
+++ reindev/assets/js/reindev.js
@@ -0,0 +1,38 @@
+document.querySelectorAll("[data-toggle-modal]").forEach(function (element) {
+  const targetId = element.getAttribute("data-toggle-modal");
+  const target = document.querySelector(targetId);
+
+  if (!target) return;
+
+  element.onclick = function () {
+    const active = target.classList.contains("active");
+
+    document.body.style.overflowY = !active ? "hidden" : "";
+
+    if (active) {
+      target.onclick = null;
+    } else {
+      target.classList.add("active");
+    }
+    if (active) {
+      target.classList.remove("active");
+    }
+  };
+});
+
+document.querySelectorAll(".collapsible").forEach(function (element) {
+  element.addEventListener("click", function (event) {
+    if (!event.target.parentNode.classList.contains("collapsible-menu")) {
+      element.classList.toggle("active");
+    }
+  });
+});
+
+const audio = new Audio("/assets/audio/click.mp3");
+const buttons = document.querySelectorAll("[data-play-sound]");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    audio.play();
+  });
+});
